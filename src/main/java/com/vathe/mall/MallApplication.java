f497fc66c273b4bf164bdb0891cd90ce288dfd6b
vathe <vatheli@eboncn.com>
2018-12-19 10:31:06 +0800

    second commit

--- src/main/java/com/vathe/mall/MallApplication.java
+++ src/main/java/com/vathe/mall/MallApplication.java
@@ -1,13 +1,17 @@
 package com.vathe.mall;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class MallApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MallApplication.class, args);
+        log.error("haha");
+
     }
 
 }
