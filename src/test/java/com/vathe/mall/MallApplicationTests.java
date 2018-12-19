d07bf61c5a6a596eb95c4f7d9019aac626151659
vathe <vatheli@eboncn.com>
2018-12-19 17:42:17 +0800

    tmp1

--- src/test/java/com/vathe/mall/MallApplicationTests.java
+++ src/test/java/com/vathe/mall/MallApplicationTests.java
@@ -1,5 +1,6 @@
 package com.vathe.mall;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class MallApplicationTests {
 
     @Test
     public void contextLoads() {
+        Assert.assertTrue(true);
     }
 
 }
